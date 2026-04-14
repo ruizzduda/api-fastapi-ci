4c6d6cc2bc99aaa4f90c7b5ea22422bee1905b4e
ruizzduda <maria.ruiz@aluno.faculdadeimpacta.com.br>
2026-04-14 15:05:50 -0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -13,4 +13,7 @@ def test_somar():
     assert response.status_code == 200
     assert response.json() == {"resultado": 8}
 
-assert response.json() == {"resultado": 4}
+def test_multiplicar():
+    response = client.get("/multiplicar/2/2")
+    assert response.status_code == 200
+    assert response.json() == {"resultado": 4}  # CERTO
